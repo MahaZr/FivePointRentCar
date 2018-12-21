fe35a12e4c3e77f6766cf995078e7976228f7bcb
hammamimariem75@gmail.com <hammamimariem75@gmail.com>
2018-12-21 08:21:27 -1100

    na7inaSelect

--- src/app/location/formulaire/FormulaireComponent.ts
+++ src/app/location/formulaire/FormulaireComponent.ts
@@ -14,13 +14,12 @@ export class FormulaireComponent implements OnInit {
   test;
   datasAero;
   public saisi = '';
-    public selectedAero = '';
+  public selectedAero = '';
   public selectedlab = '';
   public state = true;
   public actiiiiv = false;
   public tab = [];
   constructor(public data: AmadeusService) { }
-
   Save(selectedAero, pick_up, drop_off) {
     this.data.LoadData(selectedAero, pick_up, drop_off).subscribe(file => {
       console.log(file.json());
@@ -29,20 +28,18 @@ export class FormulaireComponent implements OnInit {
   }
   ngOnInit() {
   }
-
   surchaero() {
     console.log(this.saisi);
     if (this.saisi.length > 2) {
-
       this.state = true;
       this.data.getaero(this.saisi).subscribe(file1 => {
         this.test = file1.json();
         for (const i of this.test) {
           this.tab[i] = false;
         }
-
       });
-    } else {
+    }
+    else {
       this.state = false;
     }
   }
@@ -50,18 +47,11 @@ export class FormulaireComponent implements OnInit {
     this.selectedAero = value;
     this.selectedlab = value2;
     this.state = false;
-
   }
-
   mouseEnter(k) {
-
     this.tab[k] = true;
-
-
-
   }
   mouseLeave(k) {
     this.tab[k] = false;
   }
-
 }
